714d52fdf8ee6d2c4349276f7b7cabd4b30afe33
SefiShalom <33261647+SefiShalom@users.noreply.github.com>
2018-03-19 10:23:57 +0200

    Update app.js

--- app.js
+++ app.js
@@ -7,9 +7,7 @@ const path = require("path");
 const mainRequests = require('./routes/main_route');
 const searchRequests = require('./routes/search_route');
 const db = mongoose.connection;
-const mongoDBURL = "mongodb://SefiShalom:XNj3d3oVdNBMfdYi@cluster0-shard-00-00-d0iqf.mongodb.net:27017,cluster0-shard-00-01-d0iqf.mongodb.net:27017,cluster0-shard-00-02-d0iqf.mongodb.net:27017/CarsModels?replicaSet=Cluster0-shard-0&ssl=true&authSource=admin";
-
-
+const mongoDBURL = "mongodb://USERNAME:PASSWORD@cluster0-shard-00-00-d0iqf.mongodb.net:27017,cluster0-shard-00-01-d0iqf.mongodb.net:27017,cluster0-shard-00-02-d0iqf.mongodb.net:27017/CarsModels?replicaSet=Cluster0-shard-0&ssl=true&authSource=admin";
 
 mongoose.connect(mongoDBURL);
 db.on('error', console.error.bind(console, 'MongoDB connection error!\n'));
